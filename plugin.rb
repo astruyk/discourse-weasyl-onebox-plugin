42b82ffd873ca8b8d9cd1e0b43d93c99b770cd9b
Anton Struyk <astruyk@gmail.com>
2018-03-26 20:56:47 -0400

    Try capturing the variable substitution in case that's what's throwing the errors.

--- plugin.rb
+++ plugin.rb
@@ -21,35 +21,36 @@ class Onebox::Engine::WeasylSubmissionOnebox
 		imageUrl = "https://cdn.weasyl.com/static/images/logo.png";
 		iconUrl = "https://cdn.weasyl.com/static/images/favicon.png";
 
+
 		begin
 			# Weasyl exposes an HTTP API, so we can get JSON objects directly from it.
 			submissionId = @url.match(REGEX)[:id];
 			api_submissionUrl = "https://www.weasyl.com/api/submissions/#{submissionId}/view"
 			title = api_submissionUrl;
 			json = open(api_submissionUrl).read;
-			#result = JSON.parse(json);
+			result = JSON.parse(json);
 
-			description = json;
+			description = result;
 			#title = result["title"];
 			#imageUrl = result.dig('media', 'thumbnail', 'url');
-		rescue StandardError => err
+
+			<<-HTML
+				<aside class="onebox whitelistedgeneric">
+					<header class="source">
+						<img src="#{iconUrl}" class="site-icon" style="width: 16px; height: 16px" >
+						<a href="#{linkUrl}" target="_blank" rel="nofollow noopener">weasyl.com</a>
+					</header>
+					<article class="onebox-body">
+						<img src="#{imageUrl}" class="thumbnail size-resolved" />
+						<h3><a href="#{linkUrl}" target="_blank" rel="nofollow noopener">#{title}</a></h3>
+						<p>#{description}</p>
+						<div style="clear: both"></div>
+					</article>
+				</aside>
+			HTML
+		rescue => err
 			title = "Error";
 			description = err.message + "\n\n" + err.backtrace;
 		end
-
-		<<-HTML
-			<aside class="onebox whitelistedgeneric">
-				<header class="source">
-					<img src="#{iconUrl}" class="site-icon" style="width: 16px; height: 16px" >
-					<a href="#{linkUrl}" target="_blank" rel="nofollow noopener">weasyl.com</a>
-				</header>
-				<article class="onebox-body">
-					<img src="#{imageUrl}" class="thumbnail size-resolved" />
-					<h3><a href="#{linkUrl}" target="_blank" rel="nofollow noopener">#{title}</a></h3>
-					<p>#{description}</p>
-					<div style="clear: both"></div>
-				</article>
-        	</aside>
-        HTML
 	end
 end
